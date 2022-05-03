c0d6a79d3122ee17483b016d80dd8032f3b1cf7b
ldeatherage <ldeatherage>
2022-05-03 08:33:10 +0000

    Committing tests for model 'Test'

--- cypress/integration/examples/Test_FlipKartE2E4.js
+++ cypress/integration/examples/Test_FlipKartE2E4.js
@@ -5,11 +5,18 @@ describe("Test", function() {
  	
  	cy.visit('https://www.flipkart.com/')
 
-cy.get("INPUT[title='Search\\ for\\ products\\,\\ brands\\ and\\ more']").type("Carrot")
+cy.get("INPUT[title='Search\\ for\\ products\\,\\ brands\\ and\\ more']").type("iPhone 13")
 
 cy.get("BUTTON[class='L0Z3Pu']").click()
 
-cy.visit('https://www.flipkart.com/carrot-250-g/p/itmdddc23dbfe70c?pid=VEGFF2S8XYN8CHQA&lid=LSTVEGFF2S8XYN8CHQAB8EOW0&marketplace=FLIPKART&q=carrots&store=eat%2Flmt%2Fzhh&srno=s_1_4&otracker=search&otracker1=search&fm=Search&iid=5512522e-66d8-46f8-9b42-c172fad5a0a4.VEGFF2S8XYN8CHQA.SEARCH&ppt=sp&ppn=sp&qH=186a8b72fe696268')
+cy.visit('https://www.flipkart.com/apple-iphone-13-blue-128-gb/p/itm6c601e0a58b3c?pid=MOBG6VF5SMXPNQHG&lid=LSTMOBG6VF5SMXPNQHGL5FN51&marketplace=FLIPKART&q=iphone+13&store=tyy%2F4io&srno=s_1_1&otracker=search&otracker1=search&fm=organic&iid=dccaf694-7f5c-41a7-9ddd-a51871c86f9f.MOBG6VF5SMXPNQHG.SEARCH&ppt=hp&ppn=homepage&ssid=csoxh6p3280000001651234110712&qH=c68a3b83214bb235')
+
+
+cy.get("LI[id='swatch-1-color']").click()
+
+cy.get("A[class='_1fGeJ5\\ PP89tw']").click()
+
+cy.get("INPUT[placeholder='Enter\\ Delivery\\ Pincode']").type("686001")
 
 cy.get("BUTTON[class='_2KpZ6l\\ _2U9uOA\\ _3v1-ww\\ el-selection']").click()
 
