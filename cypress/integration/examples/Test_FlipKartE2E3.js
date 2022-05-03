ea81da4d3be568bbb38c38bd92df44bcbacc0cec
ldeatherage <ldeatherage>
2022-05-03 09:43:13 +0000

    Committing tests for model 'Test'

--- cypress/integration/examples/Test_FlipKartE2E3.js
+++ cypress/integration/examples/Test_FlipKartE2E3.js
@@ -5,11 +5,15 @@ describe("Test", function() {
  	
  	cy.visit('https://www.flipkart.com/')
 
-cy.get("INPUT[title='Search\\ for\\ products\\,\\ brands\\ and\\ more']").type("Carrot")
+cy.get("INPUT[title='Search\\ for\\ products\\,\\ brands\\ and\\ more']").type("adidas shoes")
 
 cy.get("BUTTON[class='L0Z3Pu']").click()
 
-cy.visit('https://www.flipkart.com/carrot-250-g/p/itmdddc23dbfe70c?pid=VEGFF2S8XYN8CHQA&lid=LSTVEGFF2S8XYN8CHQAB8EOW0&marketplace=FLIPKART&q=carrots&store=eat%2Flmt%2Fzhh&srno=s_1_4&otracker=search&otracker1=search&fm=Search&iid=5512522e-66d8-46f8-9b42-c172fad5a0a4.VEGFF2S8XYN8CHQA.SEARCH&ppt=sp&ppn=sp&qH=186a8b72fe696268')
+cy.visit('https://www.flipkart.com/adidas-classigy-m-running-shoes-men/p/itm24819973c7adf?pid=SHOG9XNYRSUMRVKP&lid=LSTSHOG9XNYRSUMRVKPWLBTYP&marketplace=FLIPKART&q=adidas+shoes&store=osp&srno=s_1_3&otracker=search&otracker1=search&fm=Search&iid=3f495a4f-b064-42c6-b857-85e09f44786e.SHOG9XNYRSUMRVKP.SEARCH&ppt=sp&ppn=sp&qH=09d26a302946ebcb')
+
+cy.get("LI[id='swatch-3-color']").click()
+
+cy.get("LI[id='swatch-1-size']").click()
 
 cy.get("BUTTON[class='_2KpZ6l\\ _2U9uOA\\ _3v1-ww\\ el-selection']").click()
 
