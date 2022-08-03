3dc4d941ba58caa4353c9012c12e48d79fbd9deb
ldeatherage <ldeatherage>
2022-08-03 09:22:54 +0000

    Committing tests for model 'Add_to_Cart'

---
+++ cypress/integration/examples/AddtoCart_AdditemtocartClickADDTOCARTSuccesfullyaddedproductstocart.js
@@ -0,0 +1,9 @@
+
+
+describe("Add_to_Cart", function() {
+   it("AdditemtocartClickADDTOCARTSuccesfullyaddedproductstocart", function() {
+ 	
+ 	cy.get("BUTTON[class='_2KpZ6l\\ _2U9uOA\\ _3v1-ww\\ el-selection']").click()
+
+   })
+})
